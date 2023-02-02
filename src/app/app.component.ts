b5602c4f784691898bed4ba37d0e49461546013d
padma <pkanakala@baylogictech.com>
2023-02-02 13:50:17 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import {HttpClient} from '@angular/common/http'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  location: any;
+  resData: any;
+  res:any;
+  data: any;
+  eachRecord:any = [];
+  firstDate: any;
+  load: boolean = false;
+  title = 'Weather in your city';
+
+  constructor(private httpClient: HttpClient,
+    private datePipe: DatePipe) {
+
+    }
+  
+
+  searchByCity(){
+      this.load = true
+      const locApi = `http://api.openweathermap.org/geo/1.0/direct?q=${this.location}&limit=5&appid=1635890035cbba097fd5c26c8ea672a1`;
+      this.httpClient.get(locApi).subscribe((response) => {
+        this.res = response;
+        var lat = this.res[0].lat;
+        var lon = this.res[0].lon;
+        const forecastApi = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=1635890035cbba097fd5c26c8ea672a1`;
+        this.httpClient.get(forecastApi).subscribe((response) => {          
+          this.data = response;
+          for(var eachItem of this.data?.list){
+             console.log("res", eachItem.dt_txt)
+             this.firstDate = this.datePipe.transform(eachItem.dt_txt, "dd/MM/yyyy");
+             console.log("firstDate", this.firstDate);
+             if (!this.eachRecord.find((item:any) => this.datePipe.transform(item.dt_txt, "dd/MM/yyyy") == this.firstDate)) {
+               this.eachRecord.push(eachItem)
+              }
+             console.log("this.eachRecord", this.eachRecord)
+          }  
+          this.load = false;     
+        });
+      });
+  }
+
+  getLocation(event: any){
+    this.location = event.target.value
+    console.log("location", this.location)
+  }
+}
